fc5db26b93ac66dcdf8679b26417c7890e873176
oz2oam <oz2oam@SZA0030877>
2023-03-28 09:03:21 +0200

    jooo

---
+++ 5gyak/5gyak/Entities/Ratedata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5gyak.Entities
+{
+    class Ratedata
+    {
+        public DateTime Date { get; set; }
+
+        public string  Currency { get; set; }
+
+        public decimal Value { get; set; }
+
+    }
+}
--- 5gyak/5gyak/Form1.cs
+++ 5gyak/5gyak/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _5gyak.Entities;
+using _5gyak.MNBServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,28 @@ namespace _5gyak
 {
     public partial class Form1 : Form
     {
+        BindingList<Ratedata> Rates = new BindingList<Ratedata>();
         public Form1()
         {
             InitializeComponent();
+
+            dataGridView1.DataSource = Rates;
+            Getrates();
+        }
+
+        private void Getrates()
+        {
+            MNBArfolyamServiceSoapClient mnbservice = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbservice.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
